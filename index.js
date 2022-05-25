c394a1a8acf9431f2405a5f86ad0f27f3cd61a6f
sazzad <hossensazzad1201@gmail.com>
2022-05-25 16:17:35 +0600

    token authorization

--- index.js
+++ index.js
@@ -13,6 +13,22 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' })
+        }
+        req.decoded = decoded;
+        next()
+    });
+
+
+}
 async function run() {
     try {
         await client.connect()
@@ -33,11 +49,18 @@ async function run() {
             const items = await itemCollection.findOne(query);
             res.send(items);
         })
-        app.get("/booking", async (req, res) => {
-            const user = req.query.user
-            const query = { user: user }
-            const bookings = await bookingCollection.find(query).toArray()
-            res.send(bookings)
+        app.get("/booking", verifyJWT, async (req, res) => {
+            const user = req.query.user;
+            const decodedEmail = req.decoded.email
+            if (user === decodedEmail) {
+                const query = { user: user }
+                const bookings = await bookingCollection.find(query).toArray()
+                return res.send(bookings)
+            }
+            else {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+
         })
         app.post("/booking", async (req, res) => {
             const booking = req.body
